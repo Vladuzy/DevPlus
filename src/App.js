f40a5a02cb4632333208c44d106f1ac93a346cbe
Adyson Bruno <adysonbruno@gmail.com>
2021-06-09 19:16:51 -0300

    refactor: remove Login of App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Login from "./pages/Login/index.jsx";
 import GlobalStyle from "./styles/global.js";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Login/>
     </>
   );
 }
