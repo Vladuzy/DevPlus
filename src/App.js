d29384641764d15016905f00ca612aa51881cdfc
Adyson Bruno <adysonbruno@gmail.com>
2021-06-09 17:58:21 -0300

    feat: initial settings login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Login from "./pages/Login/index.jsx";
 import GlobalStyle from "./styles/global.js";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Login/>
     </>
   );
 }
