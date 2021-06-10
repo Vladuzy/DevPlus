58db0f47a1ab668e83ec6f0ccfd4f4c18830e1f6
Rodrigo-Ferraz <rodrigoloha@gmail.com>
2021-06-10 10:16:30 -0300

    fix: resolving lastFinal conflict

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles/global'
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles/global";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-<<<<<<< HEAD
-     <BrowserRouter>
-          <App />
-      </BrowserRouter>
-=======
     <BrowserRouter>
-      <GlobalStyles/>
-        <App />
+      <GlobalStyles />
+      <App />
     </BrowserRouter>
->>>>>>> 3f9fec00906b3c1428ca076fbdf78661d14d136f
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
