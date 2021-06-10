b548dc8d55ac5d6b9a4b06fa0af8f2de3b587471
caiosampaio894 <caiosampaio894@gmail.com>
2021-06-10 16:14:39 -0300

    feat: doing HabitsProvider

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,41 @@
-import React from 'react'
-import { Container, HeaderStyles, ContactContainer, MainStyles } from './styles'
-import { FiAlignJustify } from 'react-icons/fi'
-import Buttons from '../../components/Button'
+import React from "react";
+import {
+  Container,
+  HeaderStyles,
+  ContactContainer,
+  MainStyles,
+} from "./styles";
+import { FiAlignJustify } from "react-icons/fi";
+import Buttons from "../../components/Button";
 
 export default function Home() {
-    return (
-        <Container>
-            <HeaderStyles>
-                <h4>Habbits lifes</h4>
-                <input id='check' type='checkbox'/>
-                <ContactContainer>
-                    <p>Contato</p>
-                    <p>comunidade</p>
-                    <p>Recursos</p>
-                </ContactContainer>
-                <label for='check'><FiAlignJustify/></label>
-            </HeaderStyles>
-            <MainStyles>
-                <h1>Habbits app</h1>
-                <h2>Venha fazer parte da nossa familia!</h2>
-                <p> o melhor jeito de você organizar os seus habitos e com uma otima comunidade!</p>
-                <div>
-                    <Buttons>Fazer Cadastro</Buttons>
-                    <Buttons>Fazer Login</Buttons>   
-                </div>                                                         
-            </MainStyles>
-        </Container>
-    )
+  return (
+    <Container>
+      <HeaderStyles>
+        <h4>Habbits lifes</h4>
+        <input id="check" type="checkbox" />
+        <ContactContainer>
+          <p>Contato</p>
+          <p>comunidade</p>
+          <p>Recursos</p>
+        </ContactContainer>
+        <label for="check">
+          <FiAlignJustify />
+        </label>
+      </HeaderStyles>
+      <MainStyles>
+        <h1>Habbits app</h1>
+        <h2>Venha fazer parte da nossa familia!</h2>
+        <p>
+          {" "}
+          o melhor jeito de você organizar os seus habitos e com uma otima
+          comunidade!
+        </p>
+        <div>
+          <Buttons>Fazer Cadastro</Buttons>
+          <Buttons>Fazer Login</Buttons>
+        </div>
+      </MainStyles>
+    </Container>
+  );
 }
---
+++ src/providers/Habits/index.js
@@ -0,0 +1,80 @@
+import { createContext, useEffect, useState } from "react";
+import api from "../../services";
+
+export const HabitsContext = createContext();
+
+export const HabitsProviders = ({ children }) => {
+  const createHabits = () => {
+    const data = {
+      title: "Calistenia a noite",
+      category: "Sáude",
+      difficulty: "Muito díficil",
+      frequency: "Diária",
+      achieved: false,
+      how_much_achieved: 30,
+      user: 1,
+    };
+    api
+      .post("/habits/", data, {
+        headers: {
+          Authorization: `Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjIzNzc2NzMwLCJqdGkiOiJmMWViZTk4MTgwN2Q0YzdlYmU2NDc3ZmI3YzFmN2Q5NyIsInVzZXJfaWQiOjcxOX0.lgfQ81zXE7u8uTbisp7YcdVLBbmWlqRpOpJW3EeFjE8"}`,
+        },
+      })
+      .then((response) => console.log(response));
+  };
+
+  const updateHabits = () => {
+    const data = {
+      how_much_achieved: 100,
+      achieved: true,
+    };
+    api
+      .patch("/habits/1451/", data, {
+        headers: {
+          Authorization: `Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjIzNzc2NzMwLCJqdGkiOiJmMWViZTk4MTgwN2Q0YzdlYmU2NDc3ZmI3YzFmN2Q5NyIsInVzZXJfaWQiOjcxOX0.lgfQ81zXE7u8uTbisp7YcdVLBbmWlqRpOpJW3EeFjE8"}`,
+        },
+      })
+      .then((response) => console.log(response));
+  };
+
+  const deleteHabits = () => {
+    api
+      .delete("/habits/1449/", {
+        headers: {
+          Authorization: `Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjIzNzc2NzMwLCJqdGkiOiJmMWViZTk4MTgwN2Q0YzdlYmU2NDc3ZmI3YzFmN2Q5NyIsInVzZXJfaWQiOjcxOX0.lgfQ81zXE7u8uTbisp7YcdVLBbmWlqRpOpJW3EeFjE8"}`,
+        },
+      })
+      .then((response) => console.log(response));
+  };
+
+  const getHabits = () => {
+    api
+      .get("/habits/personal/", {
+        headers: {
+          Authorization: `Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjIzNzc2NzMwLCJqdGkiOiJmMWViZTk4MTgwN2Q0YzdlYmU2NDc3ZmI3YzFmN2Q5NyIsInVzZXJfaWQiOjcxOX0.lgfQ81zXE7u8uTbisp7YcdVLBbmWlqRpOpJW3EeFjE8"}`,
+        },
+      })
+      .then((response) => {
+        const { data } = response;
+        localStorage.setItem("@User:habits", JSON.stringify(data));
+      });
+  };
+
+  useEffect(() => {
+    createHabits();
+  }, []);
+
+  useEffect(() => {
+    deleteHabits();
+  }, []);
+
+  useEffect(() => {
+    getHabits();
+  }, []);
+
+  useEffect(() => {
+    updateHabits();
+  }, []);
+
+  return <HabitsContext.Provider>{children}</HabitsContext.Provider>;
+};
