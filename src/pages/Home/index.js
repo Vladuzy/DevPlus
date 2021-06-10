8755d15f919dcc12a76fcb802e67c4de07d21070
Leandro Simões <Leandrosimoes42@gmail.com>
2021-06-10 01:05:34 -0300

    💄 Styles: header successfully styled

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { Container } from './styles'
+import { Container, HeaderStyles, ContactContainer } from './styles'
+import { FiAlignJustify } from 'react-icons/fi'
 
 export default function Home() {
     return (
         <Container>
-            
+            <HeaderStyles>
+                <h4>Habbits lifes</h4>
+                <input id='check' type='checkbox'/>
+                <ContactContainer>
+                    <p>Contato</p>
+                    <p>comunidade</p>
+                    <p>Recursos</p>
+                </ContactContainer>
+                <label for='check'><FiAlignJustify/></label>
+            </HeaderStyles>
         </Container>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,5 +5,51 @@ export const Container = styled.div `
     width: 100vw;
     height: 100vh;
     background-color: var(--cinza-escuro);
+    header > section{
+        display: none;
+    }
+`
 
+export const HeaderStyles = styled.header `
+    width: 100vw;
+    height: 80px;
+    display: flex;
+    color: var(--branco);
+    align-items: center;
+    justify-content: space-between;
+    input{
+        display:none;
+    }
+    svg{
+        display: none;
+    }
+    @media(max-width: 758px){
+        h4{
+            margin-left: 7vw;
+        }
+        svg{
+            display: inline-block;
+            font-size: 2rem;
+            margin-right: 3vw;
+            color: var(--verde);
+        }
+        input{
+            &:checked ~ section {
+                display: inline;
+            }
+        }
+    }
+`
+
+export const ContactContainer = styled.section `
+    width: 40vw;
+    height: 150px;
+    background: var(--verde);
+    border-radius: 20px;
+    margin-top: 120px;
+    p{
+        color: black;
+        margin-top: 20px;
+        margin-left: 10px;
+    }
 `
