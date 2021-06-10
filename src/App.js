aef764f198bd526889969f3528cbb02c8f27370d
Rodrigo-Ferraz <rodrigoloha@gmail.com>
2021-06-09 22:34:48 -0300

    feat: insert logical forms to register and styles in card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import GlobalStyle from "./styles/global.js";
+import FormRegister from "./pages/Register";
 
 function App() {
   return (
     <>
+      <FormRegister></FormRegister>
       <GlobalStyle />
     </>
   );
