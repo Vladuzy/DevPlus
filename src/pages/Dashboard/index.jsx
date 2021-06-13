4c46cc8259e12b0c5bd280abe6cfc9271c1b45e8
Adyson Bruno <adysonbruno@gmail.com>
2021-06-13 18:12:03 -0300

    feat: nesting goals dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -15,57 +15,69 @@ const Dashboard = () => {
   }
 
   return(
-    <MainDashboard back-color="green">
-      <HeaderContainer>
-        <h2>Habitos</h2>
-      </HeaderContainer>
-      <MainContainer>
-        <MainMenuContainer>
-          {/* <NavContainer>
-            <AnimateSharedLayout transition={{ duration: 0.5 }}>
-              <Link to={`${url}`} onClick={() => handleAnimation('ativos')} >
-                ATIVOS
-                { selected === 'ativos' && <AnimationContainer border-color="green" layoutId="underline"/>}
-              </Link>
-              <Link to={`${url}/feitos`} onClick={() => handleAnimation('feitos')}>
-                FEITOS
-                { selected === 'feitos' && <AnimationContainer border-color="green" layoutId="underline"/>}
-              </Link>
-            </AnimateSharedLayout>
-          </NavContainer>
-          <Switch>
-            <Route exact path={`${path}`}>
-              <p>AQUI VAI HABITOS ATIVOS</p>
-            </Route>
-            <Route path={`${path}/feitos`}>
-              <p>AQUI VAI HABITOS FEITOS</p>
-            </Route>
-          </Switch> */}
+    // <MainDashboard back-color="green">
+    //   <HeaderContainer>
+    //     <h2>Habitos</h2>
+    //   </HeaderContainer>
+    //   <MainContainer>
+    //     <MainMenuContainer>
+    //       <NavContainer>
+    //         <AnimateSharedLayout transition={{ duration: 0.5 }}>
+    //           <Link to={`${url}`} onClick={() => handleAnimation('ativos')} >
+    //             ATIVOS
+    //             { selected === 'ativos' && <AnimationContainer border-color="green" layoutId="underline"/>}
+    //           </Link>
+    //           <Link to={`${url}/feitos`} onClick={() => handleAnimation('feitos')}>
+    //             FEITOS
+    //             { selected === 'feitos' && <AnimationContainer border-color="green" layoutId="underline"/>}
+    //           </Link>
+    //         </AnimateSharedLayout>
+    //       </NavContainer>
+    //       <Switch>
+    //         <Route exact path={`${path}`}>
+    //           <p>AQUI VAI HABITOS ATIVOS</p>
+    //         </Route>
+    //         <Route path={`${path}/feitos`}>
+    //           <p>AQUI VAI HABITOS FEITOS</p>
+    //         </Route>
+    //       </Switch>
+    //     </MainMenuContainer>
+    //   </MainContainer>
+    // </MainDashboard>
 
-          <NavContainer>
-            <AnimateSharedLayout transition={{ duration: 0.5 }}>
-              <Link to={`${url}/groupX/metas/ativas`} onClick={() => handleAnimation('ativos')} >
-                ATIVOS
-                { selected === 'ativos' && <AnimationContainer layoutId="underline"/>}
-              </Link>
-              <Link to={`${url}/groupX/metas/feitas`} onClick={() => handleAnimation('feitos')}>
-                FEITOS
-                { selected === 'feitos' && <AnimationContainer layoutId="underline"/>}
-              </Link>
-            </AnimateSharedLayout>
-          </NavContainer>
+  <MainDashboard back-color="yellow">
+    <HeaderContainer>
+    <h2>Grupos</h2>
+    </HeaderContainer>
+    <MainContainer>
+      <MainMenuContainer>
+        <NavContainer>
 
-          <Switch>
-            <Route path={`${path}/groupX/metas/ativas`}>
-              <Goals showArchived = {false} />
-            </Route>
-            <Route path={`${path}/groupX/metas/feitas`}>
-              <Goals showArchived = {true} />
-            </Route>
-          </Switch>
-        </MainMenuContainer>
-      </MainContainer>
-    </MainDashboard>
+        <AnimateSharedLayout transition={{ duration: 0.5 }}>
+            <Link to={`${url}/groupX/metas/ativas`} onClick={() => handleAnimation('ativos')} >
+              ATIVOS
+              { selected === 'ativos' && <AnimationContainer border-color="yellow" layoutId="underline"/>}
+            </Link>
+            <Link to={`${url}/groupX/metas/feitas`} onClick={() => handleAnimation('feitos')}>
+              FEITOS
+              { selected === 'feitos' && <AnimationContainer border-color="yellow" layoutId="underline"/>}
+            </Link>
+          </AnimateSharedLayout>
+        </NavContainer>
+
+        <Switch>
+          <Route path={`${path}/groupX/metas/ativas`}>
+            <Goals showArchived = {false} />
+          </Route>
+          <Route path={`${path}/groupX/metas/feitas`}>
+            <Goals showArchived = {true} />
+          </Route>
+        </Switch>
+      </MainMenuContainer>
+    </MainContainer>
+  </MainDashboard>
+
+
   )
 }
 
